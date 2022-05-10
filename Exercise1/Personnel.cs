6bc9a8404c1fcfacab615ee6d65afe622444c429
Adam Daa <adamdaa87@gmail.com>
2022-05-10 16:44:20 +0200

    Add project files.

---
+++ Exercise1/Personnel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Personnel
+    {
+        private string name;     //Name 
+        private double wage;     //Wage
+
+        public Personnel()
+        {
+            name = "";
+            wage = 0.0;
+        }
+
+        public void ReadName()
+        {
+            //Read a line of text
+            Console.Write("What is the name of the emplyee? ");
+            name = Console.ReadLine();
+        }
+
+        public void ReadWage()
+        {
+            //Read a whole number
+            Console.Write("What is " + name + "'s wage? ");
+            string textValue = Console.ReadLine();
+            //convert string to number
+            wage = double.Parse(textValue.Trim());
+        }
+
+        public void ReadAndSaveTheData()
+        {
+            ReadName(); // Read the name value 
+            ReadWage(); // Read the wage value   
+        }
+
+        public void DisplayEmployeeInfo()   //Display all the values 
+        {
+            Console.WriteLine("-----------------The employee---------------------");
+            string textOut = "Name: " + name + " Wage: " + wage;
+            Console.WriteLine(textOut);
+            Console.WriteLine();
+
+        }
+
+        public void Start()
+        {
+            ReadAndSaveTheData();
+            DisplayEmployeeInfo();
+        }
+    }
+}
---
+++ Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str; //To save the entered string
+            bool EnterButton = false; // For checking th button "Enter" if it is pressed 
+            do
+            {
+                Console.WriteLine("Press any button to enter an emplyee's information or press 'Enter' to exit.");
+                str = Console.ReadLine(); //Allow the user to enter a string
+                EnterButton = String.Equals(str, ""); // Compare it with "Enter" button and store the logic  
+                Console.WriteLine();
+                if (EnterButton != true)  //Check out the logic if "Enter" button wasn't pressed then proceed  
+                {
+                    Personnel personalregister = new Personnel(); //Create 
+                    personalregister.Start(); //Call a method of the object to run
+                }
+                else
+                {   //Print out this sentence if the "Enter" button was pressed
+                    Console.WriteLine("OK. You pressed Enter. See you again!");
+                }
+            }
+            while (EnterButton != true);  //Keep repeating as long as the enter button has not pressed           
+        }
+    }
+}
